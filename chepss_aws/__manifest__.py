ea6c4ece69986b281d7bf405ec700cd8b49a5258
Cesar Freitez <cesar.freitez@chepss.cl>
2022-10-20 20:09:02 +0000

    [ADD] New module for storage file create

---
+++ chepss_aws/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Chepss Aws storage',
+    'sumary': """Chepss Aws storage module""",
+    'description':"""
+    Modificar
+    """,
+    'author': 'CHEPSS',
+    'website': 'https://www.chepss.cl',
+    'category': 'Storage',
+    'version': '0.1',
+    'depends': [
+        
+    ],
+    'license': 'LGPL-3',
+    'data':[
+        
+    ],
+    'demo':[
+        
+    ],
+}
